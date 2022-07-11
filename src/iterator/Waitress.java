55734f0528ea8274dbfa33a7d2dac267fa165570
Junsu Shin <junsushin4546@gmail.com>
2022-07-11 09:44:52 +0900

    feat: Add MenuTestDrive and Waitress class

---
+++ src/iterator/Waitress.java
@@ -0,0 +1,31 @@
+package iterator;
+
+public class Waitress {
+    PancakeHouseMenu pancakeHouseMenu;
+    DinerMenu dinerMenu;
+
+    public Waitress(PancakeHouseMenu pancakeHouseMenu, DinerMenu dinerMenu) {
+        this.pancakeHouseMenu = pancakeHouseMenu;
+        this.dinerMenu = dinerMenu;
+    }
+
+    public void printMenu() {
+        Iterator pancakeIterator = pancakeHouseMenu.createIterator();
+        Iterator dinerMenuIterator = dinerMenu.createIterator();
+
+        System.out.println("Menu\n----\nBreakfast Menu");
+        printMenu(pancakeIterator);
+        System.out.println(("\nLunch Menu"));
+        printMenu(dinerMenuIterator);
+    }
+
+    public void printMenu(Iterator iterator) {
+        while (iterator.hasNext()) {
+            MenuItem menuItem = iterator.next();
+            System.out.print(menuItem.getName() + ", ");
+            System.out.print(menuItem.getPrice() + " -- ");
+            System.out.println(menuItem.getDescription());
+        }
+    }
+
+}
