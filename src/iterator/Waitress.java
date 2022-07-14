c7e1dfde997b3bceec2bb8607fe22dae23ae9b8e
Junsu Shin <junsushin4546@gmail.com>
2022-07-14 23:35:52 +0900

    feat: Add cafeMenu with Map iterable

--- src/iterator/Waitress.java
+++ src/iterator/Waitress.java
@@ -4,20 +4,25 @@ import java.util.Iterator;
 public class Waitress {
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafeMenu;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
         Iterator<MenuItem> dinerMenuIterator = dinerMenu.createIterator();
+        Iterator<MenuItem> cafeMenuIterator = cafeMenu.createIterator();
 
         System.out.println("Menu\n----\nBreakfast Menu");
         printMenu(pancakeIterator);
         System.out.println(("\nLunch Menu"));
         printMenu(dinerMenuIterator);
+        System.out.println(("\nDinner Menu"));
+        printMenu(cafeMenuIterator);
     }
 
     public void printMenu(Iterator<MenuItem> iterator) {
